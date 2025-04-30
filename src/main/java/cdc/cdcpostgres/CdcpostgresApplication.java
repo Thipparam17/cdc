c3ed48a629f117bbbd7f3991dff1ab672ccbfb7e
Thipparam17 <132429238+Thipparam17@users.noreply.github.com>
2025-04-30 11:20:19 +0530

    Standard cdc changes with kafka ui

---
+++ src/main/java/cdc/cdcpostgres/CdcpostgresApplication.java
@@ -0,0 +1,13 @@
+package cdc.cdcpostgres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdcpostgresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdcpostgresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cdc/cdcpostgres/CdcpostgresApplicationTests.java
@@ -0,0 +1,13 @@
+package cdc.cdcpostgres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdcpostgresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
